fa6fe98a35385ef6ab69642250f980faeaab5ce5
ianlauyin <ianlauyin@gmail.com>
2024-01-28 01:40:51 +0800

    Updated product card can go to product detail

--- src/Component/Sub-Component/ProductCard.tsx
+++ src/Component/Sub-Component/ProductCard.tsx
@@ -1,6 +1,7 @@
 import StarsIcon from "@mui/icons-material/Stars";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import noImage from "../img/no-image.jpg";
+import { useNavigate } from "react-router-dom";
 type props = {
   handleAddWishItem: Function;
   product: {
@@ -18,17 +19,25 @@ type props = {
 
 //Need to add shopping Cart function
 export default function ProductCard({ product, handleAddWishItem }: props) {
+  const navi = useNavigate();
+
   return !product ? null : (
     <div className="card w-44 mt-3 bg-secondary shadow-xl">
       <img
-        className="m-1 h-32 object-contain"
+        className="m-1 h-32 object-contain cursor-pointer"
+        onClick={() => navi(`product/${product.id}`)}
         src={
           product.productPhotos.length ? product.productPhotos[0].url : noImage
         }
         alt={product.name}
       />
       <div className="card-body p-1">
-        <h2 className="card-title ">{product.name}</h2>
+        <h2
+          className="card-title cursor-pointer"
+          onClick={() => navi(`product/${product.id}`)}
+        >
+          {product.name}
+        </h2>
         <div className="flex">
           <p>HKD${product.price}</p>
           <p>Stocks: {product.stocks}</p>
