2208dad32a948fb5e7558e45741b5ea3790165ab
ianlauyin <ianlauyin@gmail.com>
2024-01-30 23:26:46 +0800

    Completed set up with map

---
+++ src/Component/AboutUs-Sub/GoogleMap.tsx
@@ -0,0 +1,38 @@
+import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
+import axios from "axios";
+import { useEffect, useState } from "react";
+const mapApiKey = process.env.REACT_APP_GOOGLE_MAP_API_KEY;
+type center = {
+  lat: number;
+  lng: number;
+} | null;
+
+export default function GoogleMap({ location }: { location: string }) {
+  const [center, setCenter] = useState<center>(null);
+
+  useEffect(() => {
+    const getCenter = async () => {
+      try {
+        const { data } = await axios.get(
+          `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${mapApiKey}`
+        );
+        setCenter({
+          lat: data.results[0].geometry.location.lat,
+          lng: data.results[0].geometry.location.lng,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getCenter();
+  });
+  return (
+    center && (
+      <APIProvider apiKey={mapApiKey ? mapApiKey : ""}>
+        <Map center={center} zoom={17} className="google-map">
+          <Marker position={center} />
+        </Map>
+      </APIProvider>
+    )
+  );
+}
--- src/Component/AboutUsPage.tsx
+++ src/Component/AboutUsPage.tsx
@@ -1,3 +1,9 @@
+import GoogleMap from "./AboutUs-Sub/GoogleMap";
+
 export default function AboutUsPage() {
-  return <div>AboutUsPage</div>;
+  return (
+    <div className="flex flex-col items-center min-h-screen">
+      <GoogleMap location={"觀塘巧明工業大廈88樓8A室"} />
+    </div>
+  );
 }
