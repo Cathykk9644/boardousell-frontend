dd9ff6be19bdc7597ad8f4c5e645bfde1846d0a8
ianlauyin <ianlauyin@gmail.com>
2024-02-01 15:29:20 +0800

    Add sending message

--- src/Component/OrderPage.tsx
+++ src/Component/OrderPage.tsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import SendIcon from "@mui/icons-material/Send";
 import {
+  Link,
   Params,
   useNavigate,
   useOutletContext,
@@ -14,7 +16,7 @@ type order = {
   address: string;
   amount: number;
   status: string;
-  createAt: string;
+  createdAt: string;
   updatedAt: string;
 };
 
@@ -35,9 +37,10 @@ type product = {
 };
 
 type message = {
+  id: number;
   isUserReceive: boolean;
   detail: string;
-  createAt: string;
+  createdAt: string;
   updatedAt: string;
 };
 
@@ -54,24 +57,24 @@ export default function OrderPage() {
   const [userInfo, setUserInfo] = useState<user>();
   const [productList, setProductList] = useState<product[]>([]);
   const [messageList, setMessageList] = useState<message[]>([]);
+  const [input, setInput] = useState<string>("");
   const navi = useNavigate();
   const { userId } = useOutletContext<outletProps>();
-  console.log(orderInfo);
-  console.log(userInfo);
-  console.log(productList);
-  console.log(messageList);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const { data } = await axios.get(`${BACKENDURL}/order/${orderId}`);
-        if (data.userId !== userId) {
+        if (!data || data.userId !== userId) {
           return navi(`/`);
         }
         const { user, products, messages, ...order } = data;
         setOrderInfo(order);
         setUserInfo(user);
         setProductList(products);
+        messages.sort((a: message, b: message) => {
+          return Date.parse(a.createdAt) - Date.parse(b.createdAt);
+        });
         setMessageList(messages);
       } catch (error) {
         console.log(error);
@@ -80,44 +83,140 @@ export default function OrderPage() {
     fetchData();
   }, [orderId]);
 
+  const handleSendMessage = async () => {
+    try {
+      const { data } = await axios.post(`${BACKENDURL}/message`, {
+        orderId,
+        isUserReseived: false,
+        detail: input,
+      });
+      setMessageList((prev) => [...prev, data]);
+      setInput("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const productDisplay = productList.map((item) => {
+    return (
+      <tr key={item.id}>
+        <td>
+          <Link to={`/product/${item.id}`} className="max-w-min">
+            {item.name}
+          </Link>
+        </td>
+        <td>x{item.productorder.amount}</td>
+      </tr>
+    );
+  });
+
+  const orderInfoDislay = (
+    <table className="table m-2">
+      <tbody>
+        <tr>
+          <th>Ordering Date:</th>
+          <td>{orderInfo?.createdAt.substring(0, 10)}</td>
+        </tr>
+        <tr>
+          <th>Status:</th>
+          <td>{orderInfo?.status}</td>
+        </tr>
+        <tr>
+          <th>Shipping Address: </th>
+          <td>{orderInfo?.address}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+
+  const membershipDisplay = (
+    <div className="m-5 w-5/6 flex flex-col">
+      You are in {userInfo?.level.title} membership now.
+      <div className="flex flex-col w-full">
+        <div className="w-full flex items-center">
+          <progress
+            className="progress progress-primary"
+            value={userInfo?.points}
+            max={userInfo?.level.requirement}
+          />
+          <span className="pl-1">
+            {userInfo?.points}/{userInfo?.level.requirement}
+          </span>
+        </div>
+        <div className="w-full flex items-center">
+          <progress
+            className="progress progress-secondary
+            "
+            value={
+              (orderInfo ? orderInfo.amount : 0) +
+              (userInfo ? userInfo.points : 0)
+            }
+            max={userInfo?.level.requirement}
+          />
+          <span className="pl-1">
+            {(orderInfo ? orderInfo.amount : 0) +
+              (userInfo ? userInfo.points : 0)}
+            /{userInfo?.level.requirement}
+          </span>
+        </div>
+        Upgrate after hitting {userInfo?.level.requirement} points.
+        <p>Points will add to your account after payment confirmed.</p>
+      </div>
+    </div>
+  );
+
+  const messageDisplay = messageList.map((message) => {
+    return (
+      <div
+        className={`chat ${message.isUserReceive ? "chat-start" : "chat-end"}`}
+        key={message.id}
+      >
+        <div className="chat-bubble chat-bubble-accent">{message.detail}</div>
+      </div>
+    );
+  });
+
+  const paymentDisplay = <div>payment</div>;
+
   return (
     <div className="min-h-screen flex flex-col">
-      <div className="w-5/6 h-5/6 bg-base-300 self-center">
-        <table>
+      <div className="m-5 border border-primary w-5/6 h-5/6 bg-base-300 self-center">
+        <table className="table border border-secondary">
           <thead>
-            <tr>Product</tr>
-            <tr>Amount</tr>
+            <tr className="border border-secondary">
+              <th>Product</th>
+              <th>Amount</th>
+            </tr>
           </thead>
+          <tbody>{productDisplay}</tbody>
         </table>
-      </div>
-      {/* <div className="w-5/6 flex flex-col">
-        You are in {userInfo?.level.title} membership now.
-        <div className="flex flex-col w-full">
-          <div className="w-full flex items-center">
-            <progress
-              className="progress progress-primary"
-              value={userInfo?.points}
-              max={userInfo?.level.requirement}
+        {orderInfoDislay}
+        {orderInfo?.status === "pending" && membershipDisplay}
+        {<div className="divider">Message:</div>}
+        <div
+          className={`m-3 bg-base-200 border-info ${
+            !!messageList.length && "border"
+          }`}
+        >
+          {!!messageList.length && messageDisplay}
+          <div className="w-full flex flex-row items-center">
+            <input
+              className="m-1 input input-md w-full"
+              type="input"
+              placeholder="Type in message here..."
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
             />
-            <span className="pl-1">
-              {userInfo?.points}/{userInfo?.level.requirement}
-            </span>
+            <button
+              className="m-1 btn btn-md btn-primary btn-square"
+              onClick={handleSendMessage}
+            >
+              <SendIcon />
+            </button>
           </div>
-          <div className="w-full flex items-center">
-            <progress
-              className="progress progress-secondary
-            "
-              value={discountedAmount + (userInfo ? userInfo.points : 0)}
-              max={userInfo?.level.requirement}
-            />
-            <span className="pl-1">
-              {discountedAmount + (userInfo ? userInfo.points : 0)}/
-              {userInfo?.level.requirement}
-            </span>
-          </div>
-          Upgrate after hitting {userInfo?.level.requirement} points.
         </div>
-      </div> */}
+        {orderInfo?.status === "pending" && paymentDisplay}
+      </div>
     </div>
   );
 }
