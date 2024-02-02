a128106c4b5eb58df42b9d47aea1c3169b184ab0
ianlauyin <ianlauyin@gmail.com>
2024-02-02 22:31:23 +0800

    Can fetch Data in explore Page

--- src/Component/ExplorePage.tsx
+++ src/Component/ExplorePage.tsx
@@ -1,3 +1,55 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useOutletContext } from "react-router-dom";
+import { BACKENDURL } from "../constant";
+
+type outletProps = {
+  userId: number;
+  handleAddWishItem: Function;
+  handleAddCart: Function;
+  handleDeleteCart: Function;
+};
+
+type product = {
+  id: number;
+  price: number;
+  name: string;
+  stocks: number;
+  onsale?: {
+    discount: number;
+  };
+  productPhotos: [
+    {
+      url?: string;
+    }
+  ];
+};
+
 export default function ExplorePage() {
-  return <div></div>;
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [onsaleList, setOnsaleList] = useState<product[]>([]);
+  const [categories, setCategories] = useState<string[]>([]);
+  const [productLists, setProductLists] = useState<product[][]>([]);
+  const { handleAddWishItem, handleAddCart } = useOutletContext<outletProps>();
+  console.log(onsaleList);
+  console.log(categories);
+  console.log(productLists);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        const { data } = await axios.get(`${BACKENDURL}/category/explore`);
+        const onsaleRes = await axios.get(`${BACKENDURL}/product/onsale`);
+        setCategories(data.categories);
+        setProductLists(data.productLists);
+        setOnsaleList(onsaleRes.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  });
+  return <div>Explore</div>;
 }
