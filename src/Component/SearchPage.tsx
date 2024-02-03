a23856b547a4f2ce41f09ac7c47daede7d75e906
ianlauyin <ianlauyin@gmail.com>
2024-02-04 00:54:56 +0800

    Can Fecth Data in Search Page

--- src/Component/SearchPage.tsx
+++ src/Component/SearchPage.tsx
@@ -1,11 +1,24 @@
-import { useEffect } from "react";
-import { useSearchParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { BACKENDURL } from "../constant";
 
 export default function SearchPage() {
-  const [searchParams] = useSearchParams();
-
-  for (const [key, value] of searchParams.entries()) {
-    console.log(key, value);
-  }
+  const [products, setProducts] = useState([]);
+  const location = useLocation();
+  console.log(products);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get(
+          `${BACKENDURL}/product/search${location.search}`
+        );
+        setProducts(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [location]);
   return <div></div>;
 }
