14c666f42e36b75719d1b642c75ed0e7617b8f72
ianlauyin <ianlauyin@gmail.com>
2024-02-09 16:04:01 +0800

    Added delete Info

--- src/Component/Admin-Sub/AdminInfoPage.tsx
+++ src/Component/Admin-Sub/AdminInfoPage.tsx
@@ -1,4 +1,9 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
+import { BACKENDURL } from "../../constant";
+
+import DeleteIcon from "@mui/icons-material/Delete";
+import { CircularProgress } from "@mui/material";
 
 type info = {
   id: number;
@@ -10,7 +15,7 @@ type info = {
 export default function AdminInfoPage() {
   const [infos, setInfos] = useState<info[]>([]);
   const [errMsg, setErrMsg] = useState("");
-  const [isLoading, setIsLoading] = useState();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
     if (errMsg.length) {
@@ -21,14 +26,73 @@ export default function AdminInfoPage() {
     }
   }, [errMsg]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        const { data } = await axios.get(`${BACKENDURL}/infomation/admin`);
+        setInfos(data);
+        setIsLoading(false);
+      } catch (error) {
+        setErrMsg("Cannot get data, please try again.");
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const deleteInfo = async (infoId: number) => {
+    try {
+      await axios.delete(`${BACKENDURL}/infomation/${infoId}`);
+      setInfos((prev: info[]) => prev.filter((info) => info.id !== infoId));
+      setErrMsg("");
+      setIsLoading(false);
+    } catch (error) {
+      setErrMsg("Cannot dalete data, please try again.");
+      setIsLoading(false);
+    }
+  };
+
   const infoDisplay = infos.map((info) => {
-    return <div></div>;
+    return (
+      <tr key={info.id}>
+        <th>{info.name}</th>
+        <td>
+          <span
+            className={
+              info.name === "Link" || info.name === "Map"
+                ? "btn btn-link pl-0"
+                : ""
+            }
+          >
+            {info.detail}
+          </span>
+        </td>
+        <td>
+          <button
+            className="btn btn-square btn-outline btn-sm"
+            onClick={() => deleteInfo(info.id)}
+          >
+            <DeleteIcon />
+          </button>
+        </td>
+      </tr>
+    );
   });
 
   return (
     <div className="flex flex-col items-center">
       {!!errMsg.length && <span className="text-error m-1 ">{errMsg}</span>}
-      <div className="flex flex-col items-center w-5/6">{infoDisplay}</div>
+      {isLoading ? (
+        <CircularProgress />
+      ) : (
+        <div className="w-5/6">
+          <button className="btn w-full btn-outline">Add Infomation</button>
+          <table className="table">
+            <tbody>{infoDisplay}</tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 }
